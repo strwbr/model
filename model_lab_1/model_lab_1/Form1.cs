dc77f8f9f80c177fe1e350c173d7e949ff4cd23b
strwbr <borisova25122001@gmail.com>
2023-02-22 15:58:40 +0400

    refactor redrawing stack and added selection

--- model_lab_1/model_lab_1/Form1.cs
+++ model_lab_1/model_lab_1/Form1.cs
@@ -72,7 +72,7 @@ namespace model_lab_1
 
         private void createBtn_Click(object sender, EventArgs e)
         {
-           // calculatorForm = new Form2();
+            // calculatorForm = new Form2();
             calculatorForm.ShowDialog();
         }
 
@@ -91,27 +91,29 @@ namespace model_lab_1
             byte col = 0;
             byte row = 0;
 
-            switch(currStackElem) {
-                case '|': row = 0; break;            
-                case '+': row = 1; break;            
-                case '-': row = 2; break;            
-                case '*': row = 3; break;            
-                case '/': row = 4; break;            
-                case '^': row = 5; break;            
+            switch (currStackElem)
+            {
+                case '|': row = 0; break;
+                case '+': row = 1; break;
+                case '-': row = 2; break;
+                case '*': row = 3; break;
+                case '/': row = 4; break;
+                case '^': row = 5; break;
                 case '(': row = 6; break;
                 case 'A': row = 7; break;
                 case 'S': row = 7; break;
                 case 'C': row = 7; break;
                 case 'T': row = 7; break;
-            }   
-            
-            switch(currInputStrElem) {
-                case '|': col = 0; break;            
-                case '+': col = 1; break;            
-                case '-': col = 2; break;            
-                case '*': col = 3; break;            
-                case '/': col = 4; break;            
-                case '^': col = 5; break;            
+            }
+
+            switch (currInputStrElem)
+            {
+                case '|': col = 0; break;
+                case '+': col = 1; break;
+                case '-': col = 2; break;
+                case '*': col = 3; break;
+                case '/': col = 4; break;
+                case '^': col = 5; break;
                 case '(': col = 6; break;
                 case ')': col = 7; break;
                 case 'A': col = 8; break;
@@ -124,13 +126,13 @@ namespace model_lab_1
             indexOperation = DijkstraAlgorithm.DijkstraTable[row, col];
             this.decisionTable.Rows[row].Cells[col].Selected = true;
 
-            switch(indexOperation)
+            switch (indexOperation)
             {
                 case 0: // ...
-                    break; 
+                    break;
                 case 1:
                     Operation1(currInputStrElem);
-                    InfixSymbols = (InfixSymbols.Length != 0) ? InfixSymbols.Substring(1) : "";
+                    //  InfixSymbols = (InfixSymbols.Length != 0) ? InfixSymbols.Substring(1) : "";
                     RedrawStack();
                     break;
                 case 2:
@@ -146,83 +148,87 @@ namespace model_lab_1
                     //больше не еш
                     //МОЖЕТ то что под свитч кейс уронит код!!!!
                     //beatBtn.Enabled = false;
-                    MessageBox.Show("Это успехххххх");
+                    MessageBox.Show("Успешное окончание преобразования");
                     break;
                 case 5:// ...
                     MessageBox.Show("Ошибка скобочной структуры!");
                     break;
                 case 6:
                     Operation6(currInputStrElem);
-                    InfixSymbols = (InfixSymbols.Length != 0) ? InfixSymbols.Substring(1) : "";
-                    //RedrawStack();
+                    //     InfixSymbols = (InfixSymbols.Length != 0) ? InfixSymbols.Substring(1) : "";
+                    RedrawStack();
                     break;
                 case 7:
-                    MessageBox.Show("После функции отсутствует '('");
+                    MessageBox.Show("Ошибка: после функции отсутствует '('");
                     break;
             }
             //RedrawStack();
 
             //infixText.Text = InfixSymbols.Substring(indexSymbol);
             //indexSymbol++;
-            
+
             //если ) то удалять нельзя
             //раскидать сабстринг по каждому кейсу (в третий точно не надо)
-           // if(!InfixSymbols.StartsWith(")") && !oper3) InfixSymbols = (InfixSymbols.Length!=0) ? InfixSymbols.Substring(1) : "";
-            
+            // if(!InfixSymbols.StartsWith(")") && !oper3) InfixSymbols = (InfixSymbols.Length!=0) ? InfixSymbols.Substring(1) : "";
+
             postfixText.Text = PostfixLine;
             infixText.Text = InfixSymbols;
         }
 
         private void RedrawStack()
         {
-            if (stack.Count != 0)
-            {     
-                // Очистка стека на форме
-                for (byte i = 0; i < stackForm.Rows.Count; i++)
-                {
-                    stackForm.Rows[i].Cells[0].Value = "";
-                }
-
-                Stack<char> copy = new Stack<char>(stack);
-
-                // Добавляет новые строки в стек на форме, если необходимо
-                // не тестила - мб улетит:)
-                if (stack.Count > stackForm.RowCount)
-                    stackForm.Rows.Add(stack.Count - stackForm.RowCount);
-
-                //надеюсь стек выжил
-                // copy = (Stack<char>)copy.Reverse();
-                for (byte i = 0; i < stack.Count; i++)
-                {
-                    stackForm.Rows[stackForm.Rows.Count - 1 - i].Cells[0].Value = copy.Pop();
-                } 
+            //if (stack.Count != 0)
+            //{
+            // Очистка стека на форме
+            for (byte i = 0; i < stackForm.Rows.Count; i++)
+            {
+                stackForm.Rows[i].Cells[0].Value = "";
             }
+
+            Stack<char> copy = new Stack<char>(stack);
+
+            // Добавляет новые строки в стек на форме, если необходимо
+            // не тестила - мб улетит:)
+            if (stack.Count > stackForm.RowCount)
+                stackForm.Rows.Add(stack.Count - stackForm.RowCount);
+
+            //надеюсь стек выжил
+            // copy = (Stack<char>)copy.Reverse();
+            for (byte i = 0; i < stack.Count; i++)
+            {
+                stackForm.Rows[stackForm.Rows.Count - 1 - i].Cells[0].Value = copy.Pop();
+            }
+
+            if (stack.Count != 0)
+                stackForm.Rows[stackForm.RowCount - stack.Count].Cells[0].Selected = true;
+            //}
         }
 
         // 1 – поместить символ из входной строки в стек
         public void Operation1(char symbol)
         {
             stack.Push(symbol);
+            InfixSymbols = (InfixSymbols.Length != 0) ? InfixSymbols.Substring(1) : "";
         }
 
         // 2 – извлечь символ из стека и отправить его в выходную строку
         public void Operation2()
         {
             //if (stack.Peek() == '(')
-            
+
             //    stack.Pop();
             //else
-                PostfixLine += stack.Pop();
+            PostfixLine += stack.Pop();
         }
 
         // 3 – удалить символ ")" из входной строки и символ "(" из стека
         public void Operation3()
         {
             if (stack.Peek() == '(') // можно и без проверки обойтись
-                stack.Pop(); 
+                stack.Pop();
 
             InfixSymbols = InfixSymbols.Substring(InfixSymbols.IndexOf(')') + 1);
-            
+
             //PostfixLine += stack.Pop();
         }
 
@@ -230,6 +236,7 @@ namespace model_lab_1
         public void Operation6(char symbol)
         {
             PostfixLine += symbol;
-        } 
+            InfixSymbols = (InfixSymbols.Length != 0) ? InfixSymbols.Substring(1) : "";
+        }
     }
 }
